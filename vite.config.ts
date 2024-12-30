0a1122c7671d7e07f4e9f855f80c5c14a965f749
dbradley2003 <dombradley2003@gmail.com>
2024-12-29 20:16:40 -0600

    fix base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/my-web-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
